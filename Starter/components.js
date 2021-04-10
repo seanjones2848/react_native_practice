e00469458c39af3171ba022e2f2c450a878ba0dd
Sean Jones <seanjones2848@gmail.com>
2021-04-09 19:39:51 -0700

    calming the signs down

--- Starter/components.js
+++ Starter/components.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 export function Zodiac(props) {
+    const [isExcited, setIsExcited] = useState(true);
+    const excitedString = "Calm this " + props.sign + " down!";
     return (
         <View style={styles.container}>
             <Text>
-                This {props.sign} sign is feeling {props.attribute}!
+                This {props.sign} sign is feeling {props.attribute} and {isExcited ? "Excited!": "Calm."}
             </Text>
+            <Button
+                onPress={() => {
+                    setIsExcited(false);
+                }}
+                disabled={!isExcited}
+                title={isExcited ? excitedString : "Thank you."}
+            />
         </View>
     )
 }
